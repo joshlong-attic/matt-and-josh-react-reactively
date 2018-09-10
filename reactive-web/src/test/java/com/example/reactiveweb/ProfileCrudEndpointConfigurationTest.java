31df2916e30ed978b427bf69303b8ec65988bb51
Josh Long <josh@joshlong.com>
2018-09-09 19:13:11 -0700

    optimize

--- reactive-web/src/test/java/com/example/reactiveweb/ProfileCrudEndpointConfigurationTest.java
+++ reactive-web/src/test/java/com/example/reactiveweb/ProfileCrudEndpointConfigurationTest.java
@@ -7,7 +7,6 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
