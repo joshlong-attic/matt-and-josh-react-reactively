ad1e6871df2348c19699c1ac2a3faa4ebe31aa2b
Josh Long <josh@joshlong.com>
2018-09-09 22:22:53 -0700

    tidiness.

--- reactive-web/src/main/java/com/example/reactiveweb/ProfileRepository.java
+++ reactive-web/src/main/java/com/example/reactiveweb/ProfileRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 	* @author <a href="mailto:josh@joshlong.com">Josh Long</a>
 	*/
 interface ProfileRepository extends ReactiveMongoRepository<Profile, String> {
+//		Flux<Profile> findByEmail(String email);
 }
