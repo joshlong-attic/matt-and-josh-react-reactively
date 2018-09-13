20e45f6a660f6ff3a2073befed8b5c93f1b9e853
Josh Long <josh@joshlong.com>
2018-09-14 02:43:05 +0300

    demonstrate synchronous and asynchronous IO.

--- reactive-web/src/main/java/com/example/reactiveweb/IoDemo.java
+++ reactive-web/src/main/java/com/example/reactiveweb/IoDemo.java
@@ -17,7 +17,7 @@ import java.nio.file.StandardOpenOption;
 import java.util.function.Consumer;
 
 interface Reader {
-		void read(File file) throws IOException;
+		void read(File file, Consumer<BytesPayload> consumer) throws IOException;
 }
 
 /**
@@ -35,18 +35,42 @@ public class IoDemo {
 						Consumer<BytesPayload> consumer = (bytes) ->
 							log.info(String.format("bytes available! got %d bytes.", bytes.getLength()));
 
-						log.info("--------------------------------------------------------------");
-						Reader synchronous = new Synchronous(consumer);
-						synchronous.read(inputFile);
+						Io io = new Io();
 
-						log.info("--------------------------------------------------------------");
-						Reader asynchronous = new Asynchronous(consumer);
-						asynchronous.read(inputFile);
+						log.info("---------------------------------");
+						io.synchronousRead(inputFile, consumer);
+
+						log.info("---------------------------------");
+						io.asynchronousRead(inputFile, consumer);
 				}
 				catch (Exception e) {
 						ReflectionUtils.rethrowRuntimeException(e);
 				}
 				System.in.read();
+		}
+}
+
+class Io {
+
+		private final Synchronous synchronous = new Synchronous();
+
+		public void synchronousRead(File f, Consumer<BytesPayload> consumer) {
+				try {
+						this.synchronous.read(f, consumer);
+				}
+				catch (IOException e) {
+						ReflectionUtils.rethrowRuntimeException(e);
+				}
+		}
+
+		public void asynchronousRead(File f, Consumer<BytesPayload> consumer) {
+				try {
+						Asynchronous asynchronous = new Asynchronous();
+						asynchronous.read(f, consumer);
+				}
+				catch (Exception ex) {
+						ReflectionUtils.rethrowRuntimeException(ex);
+				}
 		}
 }
 
@@ -65,67 +89,59 @@ class BytesPayload {
 @Log4j2
 class Synchronous implements Reader {
 
-		private final Consumer<BytesPayload> consumer;
-
-		Synchronous(Consumer<BytesPayload> consumer) {
-				this.consumer = consumer;
-		}
-
-		public void read(File file) throws IOException {
+		@Override
+		public void read(File file, Consumer<BytesPayload> consumer) throws IOException {
 				try (FileInputStream in = new FileInputStream(file)) {
 						byte[] data = new byte[FileCopyUtils.BUFFER_SIZE];
 						int res;
 						while ((res = in.read(data, 0, data.length)) != -1) {
-								this.consumer.accept(BytesPayload.from(data, res));
+								consumer.accept(BytesPayload.from(data, res));
 						}
 				}
 		}
 }
 
-class Asynchronous implements Reader {
+@Log4j2
+class Asynchronous implements Reader, CompletionHandler<Integer, ByteBuffer> {
 
-		private int bytesRead = 0;
-		private long position = 0;
-		private AsynchronousFileChannel fileChannel = null;
+		private int bytesRead;
+		private long position;
+		private AsynchronousFileChannel fileChannel;
+		private Consumer<BytesPayload> consumer;
 
-		private final Consumer<BytesPayload> consumer;
-		private final CompletionHandler<Integer, ByteBuffer> completionHandler =
-			new CompletionHandler<Integer, ByteBuffer>() {
-
-					@Override
-					public void failed(Throwable exc, ByteBuffer attachment) {
-					}
-
-					@Override
-					public void completed(Integer result, ByteBuffer buffer) {
-							Asynchronous.this.bytesRead = result;
-							if (Asynchronous.this.bytesRead < 0)
-									return;
-
-							buffer.flip();
-
-							byte[] data = new byte[buffer.limit()];
-							buffer.get(data);
-
-							Asynchronous.this.consumer.accept(BytesPayload.from(data, data.length));
-							buffer.clear();
-							Asynchronous.this.position = Asynchronous.this.position + Asynchronous.this.bytesRead;
-							Asynchronous.this.fileChannel.read(buffer, Asynchronous.this.position, buffer, this);
-					}
-			};
-
-		Asynchronous(Consumer<BytesPayload> consumer) {
-				this.consumer = consumer;
+		@Override
+		public void failed(Throwable exc, ByteBuffer attachment) {
+				log.error(exc);
 		}
 
-		public void read(File file) throws IOException {
+		@Override
+		public void completed(Integer result, ByteBuffer buffer) {
+
+				this.bytesRead = result;
+
+				if (this.bytesRead < 0)
+						return;
+
+				buffer.flip();
+
+				byte[] data = new byte[buffer.limit()];
+				buffer.get(data);
+
+				consumer.accept(BytesPayload.from(data, data.length));
+				buffer.clear();
+				this.position = this.position + this.bytesRead;
+				this.fileChannel.read(buffer, this.position, buffer, this);
+		}
+
+		public void read(File file, Consumer<BytesPayload> c) throws IOException {
+				this.consumer = c;
 				Path path = file.toPath();
 				this.fileChannel = AsynchronousFileChannel.open(path, StandardOpenOption.READ);
 				ByteBuffer buffer = ByteBuffer.allocate(FileCopyUtils.BUFFER_SIZE);
-				this.fileChannel.read(buffer, position, buffer, completionHandler);
+				this.fileChannel.read(buffer, position, buffer, this);
 				while (this.bytesRead > 0) {
 						this.position = this.position + this.bytesRead;
-						this.fileChannel.read(buffer, this.position, buffer, this.completionHandler);
+						this.fileChannel.read(buffer, this.position, buffer, this);
 				}
 		}
 }
