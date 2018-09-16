3ab4d43d3069bd2bf3a1fd3870cc3b1766767338
Josh Long <josh@joshlong.com>
2018-09-16 06:05:33 -0700

    more on the profiles HTTP endpoints..

--- reactive-web/src/main/java/com/example/demo/ProfileEndpointConfiguration.java
+++ reactive-web/src/main/java/com/example/demo/ProfileEndpointConfiguration.java
@@ -1,89 +1,29 @@
 package com.example.demo;
 
-import org.reactivestreams.Publisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.server.RequestPredicate;
 import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
-
-import java.net.URI;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.*;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
-/**
-	* @author <a href="mailto:josh@joshlong.com">Josh Long</a>
-	*/
 @Configuration
 class ProfileEndpointConfiguration {
 
-		private final ProfileService profileService;
-
-		ProfileEndpointConfiguration(ProfileService profileService) {
-				this.profileService = profileService;
-		}
-
 		@Bean
-		RouterFunction<ServerResponse> routes() {
-				return route(i(GET("/profiles")), this::all)
-					.andRoute(i(GET("/profiles/{id}")), this::getById)
-					.andRoute(i(DELETE("/profiles/{id}")), this::deleteById)
-					.andRoute(i(POST("/profiles")), this::create)
-					.andRoute(i(PUT("/profiles/{id}")), this::updateById);
+		RouterFunction<ServerResponse> routes(ProfileEndpointHandler handler) { // <1>
+				return route(i(GET("/profiles")), handler::all) //<2>
+					.andRoute(i(GET("/profiles/{id}")), handler::getById)
+					.andRoute(i(DELETE("/profiles/{id}")), handler::deleteById) //<3>
+					.andRoute(i(POST("/profiles")), handler::create)
+					.andRoute(i(PUT("/profiles/{id}")), handler::updateById);
 		}
 
-		private Mono<ServerResponse> getById(ServerRequest r) {
-				return defaultReadResponse(this.profileService.get(id(r)));
-		}
-
-		private Mono<ServerResponse> all(ServerRequest r) {
-				return defaultReadResponse(this.profileService.all());
-		}
-
-		private Mono<ServerResponse> deleteById(ServerRequest r) {
-				return defaultReadResponse(this.profileService.delete(id(r)));
-		}
-
-		private Mono<ServerResponse> updateById(ServerRequest r) {
-				Flux<Profile> id = r.bodyToFlux(Profile.class)
-					.flatMap(p -> this.profileService.update(id(r), p.getEmail()));
-				return defaultReadResponse(id);
-		}
-
-		private static Mono<ServerResponse> defaultWriteResponse(Publisher<Profile> profiles) {
-				return Mono
-					.from(profiles)
-					.flatMap(p -> ServerResponse
-						.created(URI.create("/profiles/" + p.getId()))
-						.contentType(MediaType.APPLICATION_JSON_UTF8)
-						.build()
-					);
-		}
-
-		private Mono<ServerResponse> create(ServerRequest request) {
-				Flux<Profile> flux = request
-					.bodyToFlux(Profile.class)
-					.flatMap(toWrite -> this.profileService.create(toWrite.getEmail()));
-				return defaultWriteResponse(flux) ;
-		}
-
-		private static Mono<ServerResponse> defaultReadResponse(Publisher<Profile> profiles) {
-				return ServerResponse
-					.ok()
-					.contentType(MediaType.APPLICATION_JSON_UTF8)
-					.body(profiles, Profile.class);
-		}
-
-		private static String id(ServerRequest r) {
-				return r.pathVariable("id");
-		}
-
+		// <4>
 		private static RequestPredicate i(RequestPredicate target) {
 				return new CaseInsensitiveRequestPredicate(target);
 		}
 }
+
