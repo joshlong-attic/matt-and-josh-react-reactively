345251c74230cfcecc5d339e27fcbad4c7d69265
Josh Long <josh@joshlong.com>
2018-09-17 00:18:52 -0700

    polish

--- reactive-web/src/test/java/com/example/demo/WebSocketConfigurationTest.java
+++ reactive-web/src/test/java/com/example/demo/WebSocketConfigurationTest.java
@@ -10,9 +10,11 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.socket.WebSocketMessage;
+import org.springframework.web.reactive.socket.WebSocketSession;
 import org.springframework.web.reactive.socket.client.ReactorNettyWebSocketClient;
 import org.springframework.web.reactive.socket.client.WebSocketClient;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.net.URI;
 import java.util.UUID;
@@ -21,42 +23,58 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Log4j2
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT) // <1>
 @ExtendWith(SpringExtension.class)
 class WebSocketConfigurationTest {
 
+		// <2>
 		private final WebSocketClient socketClient = new ReactorNettyWebSocketClient();
+
+		// <3>
 		private final WebClient webClient = WebClient.builder().build();
-		private final Executor executor = Executors.newCachedThreadPool();
+
+		// <4>
+		private Profile generateRandomProfile() {
+				return new Profile(UUID.randomUUID().toString(), UUID.randomUUID().toString() + "@email.com");
+		}
 
 		@Test
 		public void testNotificationsOnUpdates() throws Exception {
 
-				int count = 10;
+				int count = 10; // <5>
+				AtomicLong counter = new AtomicLong(); //<6>
+				URI uri = URI.create("ws://localhost:8080/ws/profiles"); //<7>
 
-				Flux<Profile> written = Flux
-					.<Profile>generate(sink -> sink.next(new Profile(UUID.randomUUID().toString(), UUID.randomUUID().toString() + "@email.com")))
-					.take(count)
-					.flatMap(this::write);
+				// <8>
+				socketClient.execute(uri, (WebSocketSession session) -> {
 
-				AtomicLong counter = new AtomicLong();
+						// <9>
+						Mono<WebSocketMessage> out = Mono.just(session.textMessage("test"));
 
-				this.executor.execute(() -> socketClient
-					.execute(URI.create("ws://localhost:8080/ws/profiles"), session -> session
-						.send(Flux.just("test").map(session::textMessage))
-						.thenMany(session
+						// <10>
+						Flux<String> in = session
 							.receive()
-							.map(WebSocketMessage::getPayloadAsText))
-						.doOnNext(str -> counter.incrementAndGet())
-						.then())
-					.block()
-				);
+							.map(WebSocketMessage::getPayloadAsText);
 
+						// <11>
+						return session
+							.send(out)
+							.thenMany(in)
+							.doOnNext(str -> counter.incrementAndGet())
+							.then();
+
+				}).subscribe();
+
+				// <12>
 				Flux
-					.from(written)
+					.<Profile>generate(sink -> sink.next(generateRandomProfile()))
+					.take(count)
+					.flatMap(this::write)
 					.blockLast();
 
-				Assertions.assertThat(counter.get()).isEqualTo(count);
+				Thread.sleep(1000);
+
+				Assertions.assertThat(counter.get()).isEqualTo(count); // <13>
 		}
 
 		private Publisher<Profile> write(Profile p) {
